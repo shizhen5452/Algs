cec16fdd44287e6562d780cb14aa0a6f4def1753
shizhen5452 <shizhen5452@gmail.com>
2018-07-21 08:39:17 +0800

    Hello world!

---
+++ src/com/shizhen/Main.java
@@ -0,0 +1,10 @@
+package com.shizhen;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StdOut.println("Hello world");
+    }
+}
