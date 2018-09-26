b4105f880090c47db4b7daab158fb3884a068796
shizhen5452 <shizhen5452@gmail.com>
2018-09-26 23:11:36 +0800

    add bst algs

---
+++ src/com/shizhen/chapter_three/BST.java
@@ -0,0 +1,105 @@
+package com.shizhen.chapter_three;
+
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value val, int n) {
+            this.key = key;
+            this.val = val;
+            N = n;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.N;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        }
+        return node.val;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, Key key, Value val) {
+        if (node == null) {
+            return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+        node.N = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    public Key floor(Key key) {
+        Node node = floor(root, key);
+        if (node == null) {
+            return null;
+        }
+        return node.key;
+    }
+
+    private Node floor(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return floor(node.left, key);
+        } else if (cmp > 0) {
+            Node node1 = floor(node.right, key);
+            if (node1 == null) {
+                return node;
+            } else {
+                return node1;
+            }
+        } else {
+            return node;
+        }
+    }
+
+}
